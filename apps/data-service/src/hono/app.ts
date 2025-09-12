c15998b80f9ab401c7562fabe0a112118e5ccb24
Alex Chiu <alexchiu11@gmail.com>
2025-09-12 21:13:40 +0100

    implement routing

---
+++ apps/data-service/src/hono/app.ts
@@ -0,0 +1,27 @@
+import { getLink } from '@repo/data-ops/queries/links';
+import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
+import { Hono } from 'hono';
+import { getDestinationForCountry } from '../helpers/route-ops.js';
+
+export const App = new Hono<{ Bindings: Env }>();
+
+App.get('/:id', async (c) => {
+	const { id } = c.req.param();
+	const linkData = await getLink(id);
+
+	if (!linkData) {
+		return c.json({ error: 'Link not found' }, 404);
+	}
+
+	const cfHeader = cloudflareInfoSchema.safeParse(c.req.raw.cf);
+
+	if (!cfHeader.success) {
+		return c.text('bad cf headers', 400);
+	}
+
+	const headers = cfHeader.data;
+
+	const destination = getDestinationForCountry(linkData, headers.country);
+
+	return c.redirect(destination);
+});
